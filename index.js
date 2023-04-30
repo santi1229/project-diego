15960cd8bede7ef06d412d91729875b5e9ec035f
santi1229 <98358355+santi1229@users.noreply.github.com>
2023-04-30 13:32:02 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function calculoCargaPuntual(q, z, r){
+function calculoCargaPuntual(){
+    let q=document.getElementById("t1").value;
+    let z=document.getElementById("t2").value;
+    let r=document.getElementById("t3").value;
     let p1=parseFloat((q/z**2));
     let p2=parseFloat(3/parseFloat(2*Math.PI));
     let p3=parseFloat( (((r/z)**2)+1)**(5/2) );
     p3=parseFloat(1/p3);
     esfuerzo=p1*(p2*p3);
+    //console.log(esfuerzo);
     mostrarRespuesta(esfuerzo);
 }
 function mostrarRespuesta(respuesta){
@@ -41,25 +45,10 @@ function cargaPuntual(){
     input3.style.display="block";
 
     //Boton
-    let calcular=document.getElementById("Calcular");
+    let calcular=document.getElementById("Calcular1");
     calcular.style.display="block";
-
-    let botonCalcular = document.createElement("button");
-    botonCalcular.type = "button";
-    botonCalcular.setAttribute("id", "Calcular");
-    botonCalcular.setAttribute("class", "oculto");
-    botonCalcular.innerHTML = "Calcular";
-    /*let p=input1.value;
-    let r=input2.value;
-    let z=input3.value;
-    botonCalcular.onclick = calculoCargaPuntual(p,r,z);*/
-    let form=document.getElementById("formulario")
-    form.innerHTML="<button id='Calcular' class='oculto'>Calcular</button>";
+   
     /*
-    Que funcione el calcular
-    una funcion que devuelva el resultado
-    cargar el resultado en el textfield resultado poniendolo visible
-
     despues de recibir los datos y hacer la operacion, ocultar todo
     Se puede ocultar la respuesta apenas se le da al boton para que no quede la respuesta anterior 
 
@@ -67,6 +56,10 @@ function cargaPuntual(){
     
     Como saber cual carga selecciono
     */
+}
+
+function ocultarTodo(){
+    
 }
 
 
